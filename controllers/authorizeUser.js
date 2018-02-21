1dbaa5ffe922fc320ccdf07b828b9230fc598110
sjjrvis <sjlchougule@gmail.com>
2018-02-22 01:14:03 +0530

    inital commit

---
+++ controllers/authorizeUser.js
@@ -0,0 +1,25 @@
+const request = require('request');
+
+module.exports.authorizeUser = (req, res, next) => {
+
+	const options = {
+		method: "POST",
+		url: "http://tocstack.com:5555/login",
+		body: {
+			email: "sejal@gmail.com",
+			password: "123456"
+		},
+		json: true
+	};
+
+	request(options, (err, response, body) => {
+		if (body.status) {
+			req.userName = body.userName;
+			return next();
+		}
+		else {
+			return res.json({ status: false, message: "Fatal Authentication" })
+		}
+	});
+
+}
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const multer = require('multer');
+const authorizeUser = require('./controllers/authorizeUser').authorizeUser;
+
+const port = process.env.PORT || 1111;
+const mongoURI = 'mongodb://sejal:sejal@ds119772.mlab.com:19772/xltomon';
+global.__base = __dirname;
+
+
+if (!fs.existsSync(__base + '/fileStorage/')) {
+	fs.mkdirSync(__base + '/fileStorage/')
+}
+
+let app = express();
+app.use(express.static(__dirname + '/fileStorage'));
+
+let storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, __base + '/fileStorage/');
+	}
+});
+let upload = multer({ //multer settings
+	storage: storage
+}).single('file');
+
+
+app.post('/', authorizeUser, upload, (req, res) => {
+	if (!fs.existsSync(__base + '/fileStorage/' + req.userName)) {
+		fs.mkdirSync(__base + '/fileStorage/' + req.userName)
+	}
+	if (fs.existsSync(__base + '/fileStorage/' + req.userName + "/" + req.file.originalname)) {
+		const fileName = req.file.originalname.split(".")[0] + "-" + Date.now() + "." + req.file.originalname.split(".")[1];
+		fs.copyFileSync(req.file.path, __base + '/fileStorage/' + req.userName + "/" + fileName);
+		fs.unlinkSync(req.file.path);
+		res.json({ status: true, message: "success", url: `http://${req.headers.host}/${req.userName}/${fileName}` });
+	}
+	else {
+		fs.copyFileSync(req.file.path, __base + '/fileStorage/' + req.userName + "/" + req.file.originalname);
+		fs.unlinkSync(req.file.path);
+		res.json({ status: true, message: "success", url: `http://${req.headers.host}/${req.userName}/${req.file.originalname}` });
+	}
+});
+
+app.post('/file', authorizeUser, (req, res) => {
+	const fileArray = fs.readdirSync(`${__dirname}/fileStorage/${req.userName}`);
+	let urlArray = fileArray.map(element => {
+		return {
+			name: element,
+			url: `http://${req.headers.host}/${req.userName}/${element}`
+		}
+	});
+	res.json({ status: true, data: urlArray });
+});
+
+app.listen(port, () => {
+	console.log("Server running on port ->", port);
+});
